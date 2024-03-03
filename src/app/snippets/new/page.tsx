8a7b336feee9e31f403a64644568d74ae80820d1
Edgar Mejia <edgarinhm@hotmail.com>
2024-03-02 20:06:51 -0500

    UD-02 add snippet form styles

--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -2,27 +2,33 @@ import { locale } from "@/constants/locale";
 export default function SnippetsCreatePage() {
   return (
     <form action="">
-      <h3>{locale.SnippetCreateTitle}</h3>
-      <div className="flex gap-4">
-        <label className="w-12" htmlFor="title">
-          {locale.SnippetFormTitle}
-        </label>
-        <input
-          className="border rounded p-2 w-full"
-          type="text"
-          name="title"
-          id="title"
-        />
+      <h3 className="font-bold m-3">{locale.SnippetCreateTitle}</h3>
+      <div className="flex flex-col gap-4">
+        <div className="flex gap-4">
+          <label className="w-12" htmlFor="title">
+            {locale.SnippetFormTitle}
+          </label>
+          <input
+            className="border rounded p-2 w-full"
+            type="text"
+            name="title"
+            id="title"
+          />
+        </div>
+        <div className="flex gap-4">
+          <label className="w-12" htmlFor="code">
+            {locale.SnippetFormCode}
+          </label>
+          <textarea
+            className="border rounded p-2 w-full"
+            name="code"
+            id="code"
+          />
+        </div>
+        <button type="submit" className="rounded p-2 bg-blue-200">
+          {locale.ActionButtonCreate}
+        </button>
       </div>
-      <div className="flex gap-4">
-        <label className="w-12" htmlFor="code">
-          {locale.SnippetFormCode}
-        </label>
-        <textarea className="border rounded p-2 w-full" name="code" id="code" />
-      </div>
-      <button type="submit" className="rounded p-2 bg-blue-200">
-        {locale.ActionButtonCreate}
-      </button>
     </form>
   );
 }
